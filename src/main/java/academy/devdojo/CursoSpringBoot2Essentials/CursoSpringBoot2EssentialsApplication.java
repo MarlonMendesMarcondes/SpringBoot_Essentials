060e27185bb6620d8433d978db55a49b0dd4e423
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-08-19 10:12:55 -0300

    fate: First Commit

---
+++ src/main/java/academy/devdojo/CursoSpringBoot2Essentials/CursoSpringBoot2EssentialsApplication.java
@@ -0,0 +1,12 @@
+package academy.devdojo.CursoSpringBoot2Essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBoot2EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBoot2EssentialsApplication.class, args);
+	}
+}
