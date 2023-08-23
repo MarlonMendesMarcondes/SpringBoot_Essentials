c1ed44634635e1ed58f3fa0bed900e9a4523700e
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-08-22 23:10:03 -0300

    Handler Global

---
+++ src/main/java/academy/devdojo/CursoSpringBoot2Essentials/handler/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package academy.devdojo.CursoSpringBoot2Essentials.handler;
+
+import academy.devdojo.CursoSpringBoot2Essentials.exception.BadRequestException;
+import academy.devdojo.CursoSpringBoot2Essentials.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(bre.getMessage())
+                        .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+
+}
