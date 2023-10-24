143776c879489b5a6d4190a30a123b07ade45e88
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-10-24 00:06:32 -0300

    RestTemplate POST

--- src/main/java/academy/devdojo/CursoSpringBoot2Essentials/client/SpringClient.java
+++ src/main/java/academy/devdojo/CursoSpringBoot2Essentials/client/SpringClient.java
@@ -2,8 +2,12 @@ package academy.devdojo.CursoSpringBoot2Essentials.client;
 
 import academy.devdojo.CursoSpringBoot2Essentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -14,5 +18,34 @@ public class SpringClient {
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class,2);
 
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+
+        log.info(Arrays.toString(animes));
+        //@formatter:off
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {});
+        //@formatter:on
+        log.info(exchange.getBody());
+
+//        Anime kingdom = Anime.builder().name("kingdom").build();
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
+//        log.info("saved anime {}", kingdomSaved);
+//    }
+    Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+    ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+            HttpMethod.POST,
+            new HttpEntity<>(samuraiChamploo, createJsonHeader()),
+            Anime.class);
+
+        log.info("saved anime {}", samuraiChamplooSaved);
+}
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
+
