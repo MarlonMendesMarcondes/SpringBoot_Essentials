683b224a14df60a0ac99b0fe73a4b0b2237b6a6c
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-11-26 21:36:15 -0300

    Testes de Integração pt 2

--- src/main/java/academy/devdojo/CursoSpringBoot2Essentials/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/CursoSpringBoot2Essentials/requests/AnimePostRequestBody.java
@@ -1,13 +1,17 @@
 package academy.devdojo.CursoSpringBoot2Essentials.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     private String name;
