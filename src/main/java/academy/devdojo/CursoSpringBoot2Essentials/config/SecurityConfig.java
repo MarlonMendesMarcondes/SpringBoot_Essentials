730c4065c080a6726b337269dcd67578759bc547
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2024-01-28 18:34:55 -0300

    Spring Security pt 03 - Autenticação em memória

--- src/main/java/academy/devdojo/CursoSpringBoot2Essentials/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/CursoSpringBoot2Essentials/config/SecurityConfig.java
@@ -1,21 +1,39 @@
 package academy.devdojo.CursoSpringBoot2Essentials.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Log4j2
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * BasicAuthenticationFilter
+     * UsernamePasswordAuthenticationFilter
+     * DefaultLoginPageGeneratingFilter
+     * DefaultLogoutPageGeneratingFilter
+     * FilterSecurityInterceptor
+     * Authentication -> Authorization
+     *
+     * @paramhttp
+     * @throwsException
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+//                csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
+                .and()
+                .formLogin()
                 .and()
                 .httpBasic();
     }
@@ -23,13 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        log.info("Password encoded {}",passwordEncoder.encode("Test"));
+        log.info("Password encoded {}",passwordEncoder.encode("test"));
         auth.inMemoryAuthentication()
-                .withUser("Marlon")
+                .withUser("marlon")
                 .password(passwordEncoder.encode("mendes"))
                 .roles("USER","ADMIN")
                 .and()
-                .withUser("Marcondes")
+                .withUser("marcondes")
                 .password(passwordEncoder.encode("mendes"))
                 .roles("USER");
     }
--- src/main/java/academy/devdojo/CursoSpringBoot2Essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/CursoSpringBoot2Essentials/controller/AnimeController.java
@@ -11,6 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -41,12 +44,20 @@ public class AnimeController {
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
     }
 
+    @GetMapping(path = "by-id/{id}")
+    public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable long id,
+                                                                 @AuthenticationPrincipal UserDetails userDetails) {
+        log.info(userDetails);
+        return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
+    }
+
     @GetMapping(path = "/find")
     public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
         return ResponseEntity.ok(animeService.findByName(name));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
